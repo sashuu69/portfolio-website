b5f765d834fa01f82c21fd36d8ec0f0774d9e55e
Sashwat K <sashwat0001@gmail.com>
2023-10-10 21:01:30 +0530

    Feat: Issue #9: Add support for a webpage to render Instagram BIO page

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 from flask import Flask, render_template
 
 
-with open("data.json", encoding="UTF-8") as JSONDATA:
+with open("data/index.json", encoding="UTF-8") as JSONDATA:
     DICTDATA = json.load(JSONDATA)
 
 
@@ -21,3 +21,8 @@ app = Flask(__name__, template_folder="template")
 def index() -> str:
     """Definition to render root HTML page"""
     return render_template('index.html', **DICTDATA)
+
+@app.route("/instagram_bio")
+def instgram_bio_page() -> str:
+    """Definition to render Instagram Bio HTML page"""
+    return render_template("instagram_bio.html", **DICTDATA)
